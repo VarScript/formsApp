5c392d231ddd60382207c29a0f8393c3572a405f
varscript <fvargas.edu@gmail.com>
2024-08-21 19:51:36 -0500

    re-establecer y establecer formulario

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+const rtx5900 = {
+  name: 'RTX5900',
+  price: 34400,
+  inStorage: 5,
+}
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: ``,
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit{
   public myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
     price: [0, [Validators.required, Validators.min(0)]],
@@ -13,8 +20,14 @@ export class BasicPageComponent {
   });
   constructor(private fb: FormBuilder) {}
 
+  ngOnInit(): void {
+    this.myForm.reset(rtx5900)
+  }
+
   onSave(): void {
     if (this.myForm.invalid) return;
     console.log(this.myForm.value);
+
+    this.myForm.reset({ name: 'here', price:0 , inStorage: 0})
   }
 }
