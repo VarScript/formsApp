a48ccece5b04ab17870a0b7ba91f344450ed486d
varscript <fvargas.edu@gmail.com>
2024-08-21 19:29:15 -0500

    reactive forms firsts steps

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class BasicPageComponent {
+  public myForm: FormGroup = this.fb.group({
+    name: '',
+    price: [0],
+    inStorage: [0],
+  });
+  constructor(private fb: FormBuilder) {}
 
+  onSave(): void {
+    console.log(this.myForm.value);
+  }
 }
