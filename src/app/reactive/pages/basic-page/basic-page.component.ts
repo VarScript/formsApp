9e0ab567f592aedfe5a8a0231ac2c88f7a40f71c
varscript <fvargas.edu@gmail.com>
2024-09-08 16:07:13 -0500

    Mostrar errores en validación de pantallas

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,18 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-
 const rtx5900 = {
   name: 'RTX5900',
   price: 34400,
   inStorage: 5,
-}
+};
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: ``,
 })
-export class BasicPageComponent implements OnInit{
+export class BasicPageComponent implements OnInit {
   public myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
     price: [0, [Validators.required, Validators.min(0)]],
@@ -21,13 +20,16 @@ export class BasicPageComponent implements OnInit{
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.myForm.reset(rtx5900)
+    //this.myForm.reset(rtx5900);
   }
 
   onSave(): void {
-    if (this.myForm.invalid) return;
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
     console.log(this.myForm.value);
 
-    this.myForm.reset({ name: 'here', price:0 , inStorage: 0})
+    this.myForm.reset({ name: 'Reset make this', price: 0, inStorage: 0 });
   }
 }
